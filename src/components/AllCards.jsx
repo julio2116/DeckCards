e5a6cd9fa5dcf8b2b23f16e6954c78c4e04c269d
Jb2116 <juliobatista2116@gmail.com>
2024-11-10 10:28:09 -0300

    Ajustes na centralização dos cards

--- src/components/AllCards.jsx
+++ src/components/AllCards.jsx
@@ -3,12 +3,12 @@ import Card from '../components/Card.jsx'
 
 const AllCards = () => {
     let array = [];
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < 8; i++){
         array.push(i);
     }
     return(
         <>
-            <div className={styles.container} style={{maxWidth:`${(array.length-1)*215}px`, height:`420px`}}>
+            <div className={styles.container} style={{maxWidth:`${(array.length-1)*150+250}px`, height:`420px`}}>
                 {array.map((number, index) => (
                     <Card key={index} positionLeft={`${index*150}px`} />
                     
